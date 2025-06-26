29c846987f3f495500b3b72b51d00906f0dceb0d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-26 21:40:32 +0100

    Feat: added new links

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function UserNav() {
   return (
     <nav>
+      <Link href={"/timeline"}>Timeline</Link>
       <Link href={"/user"}>Profile Page</Link>
+      <Link href={"/posts/new"}>Make a post</Link>
     </nav>
   );
 }
