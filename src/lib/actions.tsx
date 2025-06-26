2545c6ca49f7c70fa11d5251a85a58bc204e0f08
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-26 18:28:33 +0100

    Chore: added types

--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -3,7 +3,7 @@ import { db } from "@/utils/dbConnection";
 // import { revalidatePath } from "next/cache";
 // import { redirect } from "next/navigation";
 
-export async function createProfile(id, formData) {
+export async function createProfile(id: string, formData: FormData) {
   const profileData = {
     username: formData.get("username"),
     bio: formData.get("bio"),
