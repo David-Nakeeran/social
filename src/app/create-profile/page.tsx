789832257148852863808550755df96d6958cb8f
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-26 14:39:49 +0100

    Chore: clerk setup

---
+++ src/app/create-profile/page.tsx
@@ -0,0 +1,16 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+export default async function CreateProfilePage() {
+  const { userId } = await auth();
+  const user = await currentUser();
+  console.log(userId);
+  console.log(user);
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  return (
+    <>
+      <h1>Create profile, protected...I hope</h1>
+    </>
+  );
+}
