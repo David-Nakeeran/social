98ebcf3a8736ae92931134713ae1ee1fe73021ee
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-27 14:25:43 +0100

    Chore: added ensureUserHasProfile check

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,15 +1,15 @@
 import PostForm from "@/components/PostForm";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ensureUserHasProfile } from "@/lib/guards";
 
 export default async function PostNewPage() {
   const { userId } = await auth();
-  //   const user = await currentUser();
-  //   console.log(userId);
-  //   console.log(user);
+
   if (!userId) {
     return redirect("/sign-in");
   }
+  await ensureUserHasProfile({ userId });
   return (
     <main>
       <h1>Make a new social post</h1>
