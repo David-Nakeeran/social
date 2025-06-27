f297b3c2df03e63074bdf630414f0a85bdcae54a
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-27 11:13:13 +0100

    Chore: imported date component

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/utils/dbConnection";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import LocalDate from "@/components/LocalDate";
 
 export default async function TimeLinePage() {
   const { userId } = await auth();
@@ -12,7 +13,7 @@ export default async function TimeLinePage() {
   async function getAllPosts() {
     try {
       const posts = await db.query(
-        `SELECT social_posts.content, social_posts.created_at, profiles.username
+        `SELECT social_posts.id, social_posts.content, social_posts.created_at, profiles.username
         FROM social_posts
         LEFT JOIN profiles ON social_posts.user_id = profiles.id
         ORDER BY social_posts.created_at DESC
@@ -34,7 +35,7 @@ export default async function TimeLinePage() {
       <div key={element.id}>
         <p>{element.username}</p>
         <p>{element.content}</p>
-        <p>{new Date(element.created_at).toLocaleString("en-GB")}</p>
+        <LocalDate dateString={element.created_at.toISOString()} />
       </div>
     );
   });
